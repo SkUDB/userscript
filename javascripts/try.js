1274b8822b4285154d184fff706c92240435109f
SkUDB <keshafue@gmail.com>
2015-02-25 21:45:56 +0300

    Update and rename javascripts to javascripts/try.js

---
+++ javascripts/try.js
@@ -0,0 +1,2 @@
+$(".breadBoxTop").after("<div style=\"margin-bottom: 10px; padding: 10px; color: #000000; font-size: 16px; line-height: 1.5\" class=\"breadcrumb\"><b>Новость для пользователей OS X:</b> Tampermonkey стал доступен для Safari</b></div>");
+$(".breadBoxTop").after("<div style=\"margin-bottom: 10px; padding: 10px; color: #000000; font-size: 16px; line-height: 1.5\" class=\"breadcrumb\"><b>Новость для пользователей Android:</b> Tampermonkey стал доступен для Android</b></div>");;
