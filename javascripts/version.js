347a6459d0f0812b02e3c09b1011f7edb19f6216
SkUDB <keshafue@gmail.com>
2015-02-25 22:13:10 +0300

    Update version.js

--- javascripts/version.js
+++ javascripts/version.js
@@ -1,3 +1,2 @@
 localStorage.setItem("serverVersion", "2.0.5");
 $(".breadBoxTop").after("<div style=\"margin-bottom: 10px; padding: 10px; color: #000000; font-size: 16px; line-height: 1.5\" class=\"breadcrumb\"><b>Новость для пользователей OS X:</b> Tampermonkey стал доступен для Safari</b></div>");
-$(".breadBoxTop").after("<div style=\"margin-bottom: 10px; padding: 10px; color: #000000; font-size: 16px; line-height: 1.5\" class=\"breadcrumb\"><b>Новость для пользователей Android:</b> Tampermonkey стал доступен для Android</b></div>");
