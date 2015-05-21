b81e2bca21017323d2c1fb2fb4f3e9bdf8f21568
SkUDB <keshafue@gmail.com>
2015-05-21 11:49:06 +0300

    Update version.js

--- javascripts/version.js
+++ javascripts/version.js
@@ -1,3 +1,3 @@
-localStorage.setItem("serverVersion", "2.0.15");
+localStorage.setItem("serverVersion", "2.0.16");
 //$(".breadBoxTop").after("<div style=\"margin-bottom: 10px; padding: 10px; color: orangered; font-size: 16px; line-height: 1.5; text-align: center;\" class=\"breadcrumb\"><img src=\"http://vk.com/images/emoji/D83CDF39.png\"> <b>С праздником, дорогие женщины!</b> <img src=\"http://vk.com/images/emoji/D83CDF39.png\"></b></div>");
 
