591a301309e68e76a338630a36ba09ede1479ce9
SkUDB <keshafue@gmail.com>
2015-07-14 19:10:17 +0300

    Update version.js

--- javascripts/version.js
+++ javascripts/version.js
@@ -1,3 +1,3 @@
 localStorage.setItem("serverVersion", "2.0.16");
-//$(".breadBoxTop").after("<div style=\"margin-bottom: 10px; padding: 10px; color: orangered; font-size: 16px; line-height: 1.5; text-align: center;\" class=\"breadcrumb\"><img src=\"http://vk.com/images/emoji/D83CDF39.png\"> <b>С праздником, дорогие женщины!</b> <img src=\"http://vk.com/images/emoji/D83CDF39.png\"></b></div>");
+$(".breadBoxTop").after("<div style=\"margin-bottom: 10px; padding: 10px; color: orangered; font-size: 16px; line-height: 1.5; text-align: center;\" class=\"breadcrumb\">Обновления Skladchik Userscript заморожены до 1 августа! <small>объявление отключится 17 июля</small></b></div>");
 
