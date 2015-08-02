6bb7c5a28a1e365c2e17c709d6989faf0ed5b002
SkUDB <keshafue@gmail.com>
2015-08-02 18:28:14 +0300

    Update version.js

--- javascripts/version.js
+++ javascripts/version.js
@@ -1,3 +1,3 @@
-localStorage.setItem("serverVersion", "2.0.17");
+localStorage.setItem("serverVersion", "2.0.18");
 //$(".breadBoxTop").after("<div style=\"margin-bottom: 10px; padding: 10px; color: orangered; font-size: 16px; line-height: 1.5; text-align: center;\" class=\"breadcrumb\">Обновления Skladchik Userscript заморожены до 1 августа! <small>объявление отключится 17 июля</small></b></div>");
 
